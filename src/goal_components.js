a178d03a636a502901ddd62bb2199d30363d9a1b
Matanda Phiri <matandaphiri64@gmail.com>
2025-02-21 11:48:10 -0600

    Add styles.css and start styling front end

--- src/goal_components.js
+++ src/goal_components.js
@@ -1,7 +1,15 @@
 //These functions will be used to set the Title and note values
-const setTitle = (title) => title;
-const setDescription = (description) => description;
-const setDueDate = (dueDate) => dueDate;
-const priority = (priority) =>
+class Goal{
+    constructor(title, description,priority){
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    setPriority
+}
+
+export {Goal}
+
 
 
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { Goal } from "./goal_components.js";
+
+const myNewGoal = new Goal("Make Cheesy Cheese Cheese", "I am the goat", "high");
+console.log(myNewGoal)
