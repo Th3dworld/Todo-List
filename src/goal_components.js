2d0ddd091d43f336ec267b8cac13e7b9dbf8a8c0
Matanda Phiri <matandaphiri64@gmail.com>
2025-02-21 11:09:37 -0600

    Add goal_components.js

---
+++ src/goal_components.js
@@ -0,0 +1,7 @@
+//These functions will be used to set the Title and note values
+const setTitle = (title) => title;
+const setDescription = (description) => description;
+const setDueDate = (dueDate) => dueDate;
+const priority = (priority) =>
+
+
