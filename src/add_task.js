fc100e05dde7cba5447611afd6924034e4179bd8
Matanda Phiri <matandaphiri64@gmail.com>
2025-02-22 20:59:04 -0600

    Add method to display tasks

--- src/add_task.js
+++ src/add_task.js
@@ -1,13 +1,43 @@
+import { addGoal, showTasks} from "./task_components.js";
+
 const tasks = [];
+
+tasks.push(addGoal("Workout", "Go to LA fitness and workout", "N/A", "high", "2/23/25"));  
+tasks.push(addGoal("Read", "Finish one chapter of 'Atomic Habits'", "N/A", "mid", "2/24/25"));  
+tasks.push(addGoal("Code", "Work on Dynamic Bulk Uploader project", "N/A", "high", "2/25/25"));  
+tasks.push(addGoal("Study", "Review recursion and DFS for 1 hour", "N/A", "high", "2/23/25"));  
+tasks.push(addGoal("Meditate", "Practice mindfulness for 10 minutes", "N/A", "low", "2/26/25"));  
+tasks.push(addGoal("Call", "Check in with family and have a conversation", "N/A", "mid", "2/27/25"));   
+ 
+
+
 const addTaskBtn = document.querySelector("#add-task-btn");
 const taskDialog = document.querySelector("#task-dialog");
-const submitTaskBtn = document.querySelector("#sumbit-task");
+const display = document.querySelector("#display");
 
+addTaskBtn.addEventListener("click", () =>{taskDialog.showModal()});
+taskDialog.addEventListener("close", (e) =>{
+    const title = document.querySelector("#task-title-input").value;
+    const description = document.querySelector("#task-description-input").value;
+    const project = document.querySelector("#task-project-input").value;
+    var priority;    
 
+    //get priority
+    document.getElementsByName("task-priority-input").forEach(elem => {
+        if(elem.checked){
+            priority = elem.value;
+        }
+    });
 
-addTaskBtn.addEventListener("click", () =>{taskDialog.showModal()})
-taskDialog.addEventListener("close", (e) =>{})
+    console.log(priority)
+    console.log(project)
+    console.log(description)
+    console.log(title)
 
-function getInputData(){
+    tasks.push(addGoal(title, description,project,priority, ""))
 
-}
+    showTasks(tasks);
+
+    console.log(tasks);
+});
+
--- src/task_components.js
+++ src/task_components.js
@@ -1,16 +1,39 @@
 //These functions will be used to set the Title and note values
 class Goal{
-    constructor(title, description, project, priority){
+    constructor(title, description, project, priority, dueDate){
         this.title = title;
         this.description = description;
         this.priority = priority;
         this.project = project
+        this.dueDate = dueDate
     }
 
-    setPriority
 }
 
-export {Goal}
+function addGoal(title, description, project, priority, dueDate){
+    return new Goal(title, description, project, priority, dueDate)
+}
+
+function showTasks(taskArray){
+    if(taskArray.length > 0){
+        tasks.forEach(elem => {
+            document.querySelector("#display").innerHTML += `
+            <div id="task">
+            <div id="check"><input type="checkbox"></div>
+            <div id="task-title">${elem.title}</div>
+            <div id="task-description">${elem.description}</div>
+            <div id="date">
+                <div>Due</div>
+                <div id="ac-date">${elem.dueDate}</div>
+            </div>
+            <div class="task-priority-${elem.priority}"></div>
+            </div>
+            `
+        });
+    }
+}
+
+export {addGoal, showTasks}
 
 
 
